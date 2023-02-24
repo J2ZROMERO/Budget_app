3537dfba0a088d4a63abdea8a87320ae3ec73437
Jhosep <jose-zromero@hotmail.com>
2023-02-23 18:00:16 -0600

    Styles completed

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -4,5 +4,7 @@ class Budget < ApplicationRecord
    
     has_many :budgets_groups, dependent: :destroy
     has_many :groups, through: :budgets_groups
+    validates :name, length: { maximum: 100 }, presence: { message: "The name field is mandatory" }
+    validates :amount, length: { maximum: 100 }, presence: { message: "The name field is mandatory" }
 
 end
